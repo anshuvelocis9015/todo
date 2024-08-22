02407cd7c610e294958c016bc814f78c1a78e773
Anshu Kumar <anshu.kumar@velocis.in>
2024-08-22 16:48:19 +0530

    to-do complete

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoItem = () =>{
+    const[value,setValue] = useState("");
+    const[todos, setTodos] = useState([]);
+    const[editIndex,setEditIndex] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(value === ""){
+            return console.log("add to-do please");
+        }
+        if(editIndex !== null){
+            const updatedArray = [...todos];
+            updatedArray[editIndex] = value;
+            setTodos(updatedArray);
+        }
+        else{
+        const newAdd = [...todos,value];
+        setTodos(newAdd);
+        console.log(newAdd);
+        }
+        setValue("");
+    }
+    const handleDelete = () => {
+        const newArray = [...todos];
+        newArray.splice(todos.index,1);
+        setTodos(newArray);
+    }
+    const handleEdit = (index) => {
+        setValue(todos[index]);
+        setEditIndex(index);
+    }
     return(
         <div>
-            <h3>Title</h3>
-            <p>Description</p>
+           <form onSubmit={handleSubmit}>
+            <input type="text" value={value} placeholder="Enter your todo" onChange={(e) => setValue(e.target.value)}/>
+            <button type='submit' onClick={handleSubmit}>Add To Do</button>
+           </form>
+           <ul>
+            {todos.map((todo, index) => (
+                <li key={index}>{todo}
+                <button onClick={() => handleEdit(index)}>Edit</button>
+                <button onClick={handleDelete}>Delete</button>
+                </li>
+            ))}
+           </ul>
         </div>
     )
 }
